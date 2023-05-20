716e3892d7ec8887549642217c4143d0a1ba1bdd
polnikov.andrey <polnikov.andrey1981@gmail.com>
2023-05-20 16:40:40 +0300

    + DemoSecurityConfig login page configuration + LoginController + plain-login.html

--- src/main/java/com/mcfly/springboot/demo_security/security/DemoSecurityConfig.java
+++ src/main/java/com/mcfly/springboot/demo_security/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.mcfly.springboot.demo_security.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -30,5 +32,17 @@ public class DemoSecurityConfig {
                 .roles("EMPLOYEE", "MANAGER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated())
+                .formLogin(form ->
+                        form
+                                .loginPage("/showLoginPage")
+                                .loginProcessingUrl("/authenticateUser")
+                                .permitAll())
+                .build();
     }
 }
